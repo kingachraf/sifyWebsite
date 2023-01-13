5ff45c7f8b9418dd50cf63983020994b2d43c945
Achrafgi <106438023+Achrafgi@users.noreply.github.com>
2023-01-13 17:40:09 +0000

    Ajoutez des fichiers projet.

---
+++ sifyWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sifyWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
